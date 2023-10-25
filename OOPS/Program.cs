6b9e14009c90aabc6ac63954eb818d6fc9fa3940
VijayHYadav <vizzyadav1997@gmail.com>
2023-10-26 00:07:13 +0530

    11. Reflection-Accessing Metadata Information

--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -1,7 +1,45 @@
-﻿using OOPS;
+﻿using System.Reflection;
+using OOPS;
 
-TypeCheck typeCheck = new TypeCheck();
-Employee emp = new  Employee();
-typeCheck.GetEmployee(emp);
+Employee employee = new Employee();
+Type T = employee.GetType();
 
-Console.WriteLine($"{emp.Department}");
+Console.WriteLine($"The full name of the type is {T.FullName}");
+Console.WriteLine($"The name of the type is {T.Name}");
+Console.WriteLine($"The namespace is {T.Namespace}");
+
+PropertyInfo[] properties = T.GetProperties();
+
+Console.WriteLine("Each of the properties of the type T has the following details:");
+
+foreach (PropertyInfo property in properties)
+{
+    Console.WriteLine($"Name = {property.Name}, Type = {property.PropertyType.Name}");
+}
+
+MethodInfo[] methods = T.GetMethods();
+
+Console.WriteLine("Each of the methods of the type T has the following details:");
+
+foreach (MethodInfo method in methods)
+{
+    Console.WriteLine($"Name = {method.Name}, Return Type = {method.ReturnType.Name}");
+}
+
+ConstructorInfo[] constructors = T.GetConstructors();
+
+Console.WriteLine("Each of the constructor of the type T has the following details:");
+
+foreach (ConstructorInfo constructor in constructors)
+{
+    Console.WriteLine($"Name = {constructor.Name}, Parameters = {constructor}");
+}
+
+FieldInfo[] fields = T.GetFields();
+
+Console.WriteLine("Each of the fields of the type T has the following details:");
+
+foreach (FieldInfo field in fields)
+{
+    Console.WriteLine($"Name = {field.Name}");
+}
--- OOPS/TypeCheck.cs
+++ OOPS/TypeCheck.cs
@@ -25,5 +25,31 @@ namespace OOPS
         public int Id { get; set; }
         public string Name { get; set; }
         public string Department { get; set; }
+        public int Salary { get; set; }
+
+        public Employee() {
+            this.Id = 0;
+            this.Salary = 0;
+        }
+
+        public Employee(int id, string name, string department) 
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Department = department;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("I am the print method");
+        }
+
+        public int Add()
+        {
+            int a = 9;
+            int b  = 8;
+            Console.WriteLine("I am the print method");
+            return (a + b);
+        }
     }
 }
