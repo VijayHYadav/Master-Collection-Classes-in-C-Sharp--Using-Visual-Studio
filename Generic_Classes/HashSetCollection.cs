e28c074af73cd24cc2c21b07b4cfdbf51c1b71fd
VijayHYadav <vizzyadav1997@gmail.com>
2023-10-30 08:22:01 +0530

    38. HashSet<T> Hands on Coding with Commonly Used Methods - Part 2

--- Generic_Classes/HashSetCollection.cs
+++ Generic_Classes/HashSetCollection.cs
@@ -60,6 +60,52 @@ namespace Generic_Classes
             {
                 Console.WriteLine(item);
             }
+
+            // ! Lecture 2 Starts
+            Console.WriteLine("=======================================");
+            
+            HashSet<int> setOfNumbers = new HashSet<int>();
+
+            for (int i = 0; i <= 5; i++)
+            {
+                setOfNumbers.Add(i);
+            }
+
+            Console.WriteLine($"setOfNumber contains {setOfNumbers.Count}");
+            PrintSet(setOfNumbers);
+
+            void PrintSet(HashSet<int> set)
+            {
+                Console.WriteLine("{");
+                foreach (var i in set)
+                {
+                    Console.WriteLine($" {i}");
+                }
+                Console.WriteLine(" }");
+            }
+
+            Console.WriteLine("setOfNumbers after removing the even numbers");
+            setOfNumbers.RemoveWhere(IsEven);
+            PrintSet(setOfNumbers);
+            bool IsEven(int i) {
+                return ((i % 2) == 0);
+            }
+
+            HashSet<int> setOfNumbers2 = new HashSet<int>();
+
+            for (int i = 0; i <= 5; i++)
+            {
+                setOfNumbers2.Add(i);
+            }
+            PrintSet(setOfNumbers2);
+
+            if(setOfNumbers2.Contains(0)) {
+                setOfNumbers2.Remove(0);
+            }
+            Console.WriteLine("setOfNumbers2 after removing the 0");
+
+            PrintSet(setOfNumbers2);
+
         }
     }
 }
